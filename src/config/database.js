73839c29fa8cafc6c48c96559dd330af0d41ad15
AndreaSantanaCea1999 <an.santanac@duocuc.cl>
2025-06-05 16:35:18 -0400

    cambio de base de datos

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@ const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
 const sequelize = new Sequelize(
-  process.env.DB_NAME || 'ferremax',
+  process.env.DB_NAME || 'ferremas_complete',
   process.env.DB_USER || 'root',
   process.env.DB_PASSWORD || 'password',
   {
