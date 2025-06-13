7e38d0b4412c70966f41473d820cda58f52d721b
AndreaSantanaCea1999 <an.santanac@duocuc.cl>
2025-06-12 21:07:27 -0400

    .

--- src/routes/pedidosRoutes.js
+++ src/routes/pedidosRoutes.js
@@ -2,13 +2,13 @@ const express = require('express');
 const router = express.Router();
 const pedidosController = require('../controllers/pedidosController');
 // Rutas CRUD para pedidos
-router.get('/', pedidoController.getAllPedidos);
-router.get('/estadisticas', pedidoController.getEstadisticasPedidos);
-router.get('/:id', pedidoController.getPedidoById);
-router.post('/', pedidoController.createPedido);
+router.get('/', pedidosController.getAllPedidos);
+router.get('/estadisticas', pedidosController.getEstadisticasPedidos);
+router.get('/:id', pedidosController.getPedidoById);
+router.post('/', pedidosController.createPedido);
 router.put('/:id/estado', pedidosController.updateEstadoPedido);
 router.put('/pedidos/:id/cancelar', pedidosController.cancelarPedido);
-router.patch('/:idPedido/detalles/estado', pedidosController.actualizarEstadoDetalles);
+// router.patch('/:idPedido/detalles/estado', pedidosController.actualizarEstadoDetalles); // Esta línea parece referenciar una función no existente en el controlador
 router.delete('/:id', pedidosController.cancelarPedido);
 
 module.exports = router;
